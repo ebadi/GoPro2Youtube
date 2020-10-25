d5bed9f6aca1de460abdea1b77ba6ab2e062cfc9
Hamid Ebadi <hamid.ebadi@gmail.com>
2020-10-25 22:11:35 +0100

    Overwrite previous file and instruction

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GoPro2Youtube
 Processing GoPro videos before uploading to Youtube
+
+Execute:
+
+```./videoProcess.sh /media/USER/disk/DCIM/100GOPRO/```
--- videoProcess.sh
+++ videoProcess.sh
@@ -7,7 +7,7 @@ for videofile in $videodir/*.MP4; do
     [ -f "$videofile" ] || break
     echo "======[ Processing: " $videofile "]========"
     ffmpeg $range -i $videofile -vf vidstabdetect=shakiness=5:show=1 -f null - 
-    ffmpeg $range -i $videofile -vf vidstabtransform,unsharp=5:5:0.8:3:3:0.4 $audio $videofile-stab.mp4
+    ffmpeg $range -i $videofile -y -vf vidstabtransform,unsharp=5:5:0.8:3:3:0.4 $audio $videofile-stab.mp4
 done
 
 
