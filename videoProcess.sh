2fb94f5a8d6d2921c81779d1fea2e6a19df69b4a
Hamid Ebadi <hamid.ebadi@gmail.com>
2020-10-27 14:44:11 +0100

    Instruction to install ffmpeg with the right filter for older ubuntu installations

--- videoProcess.sh
+++ videoProcess.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#Ubutnu 18.04
+#sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+#sudo apt update
+#sudo apt install ffmpeg -y
+
 videodir=$1
 #range=" -t 5 " # only the first 5 seconds, for debugging
 audio=" -an "  # removes audio
@@ -8,6 +14,7 @@ for videofile in $videodir/*.MP4; do
     echo "======[ Processing: " $videofile "]========"
     ffmpeg $range -i $videofile -vf vidstabdetect=shakiness=5:show=1 -f null - 
     ffmpeg $range -i $videofile -y -vf vidstabtransform,unsharp=5:5:0.8:3:3:0.4 $audio $videofile-stab.mp4
+    rm transforms.trf
 done
 
 
